2a2de9f834d46cd58a3b87814e05fc28c42cc219
Gustabolou <gustavosales23@outlook.com>
2025-05-25 18:22:42 -0300

    Update project settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/react-task-list/',
   plugins: [react()],
+  build: {
+    assetsDir: './assets',
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
